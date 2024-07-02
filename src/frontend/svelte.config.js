a4f4a0d645a3463500ef08ca2f23a1952e5780bd
PolpOnline <git@t0mmy.anonaddy.com>
2024-07-02 14:28:08 +0200

    Add svelte inspector

--- src/frontend/svelte.config.js
+++ src/frontend/svelte.config.js
@@ -7,6 +7,15 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+	vitePlugin: {
+		// set to true for defaults or customize with object
+		inspector: {
+			toggleKeyCombo: 'meta-shift',
+			showToggleButton: 'always',
+			toggleButtonPos: 'bottom-right'
+		}
+	},
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
