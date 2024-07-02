9dfc869b70589abacf63adb476d6b9f975d400ba
PolpOnline <git@t0mmy.anonaddy.com>
2024-07-02 15:52:37 +0200

    Add favicon

--- src/frontend/src/lib/components/Navbar.svelte
+++ src/frontend/src/lib/components/Navbar.svelte
@@ -4,6 +4,6 @@
 
 <nav class="flex h-20 flex-row items-center justify-between">
 	<PhHeartbeat class="ml-3 justify-self-start text-3xl" />
-	<span class="mx-2 justify-self-center text-3xl"> Heartbeat Monitor </span>
+	<span class="mx-2 justify-self-center text-3xl"> Monitor </span>
 	<span />
 </nav>
--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import '../app.pcss';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import favicon from '$lib/images/favicon.svg';
 </script>
+
+<svelte:head>
+	<link href={favicon} rel="icon" type="image/svg+xml" />
+</svelte:head>
 
 <Navbar />
 
