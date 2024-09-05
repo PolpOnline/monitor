6fa9f1621c394c9cd25226db9232541ecf71ea18
PolpOnline <git@t0mmy.anonaddy.com>
2024-09-05 21:07:41 +0200

    Fix ui components

--- src/frontend/src/lib/components/ui/form/form-field-errors.svelte
+++ src/frontend/src/lib/components/ui/form/form-field-errors.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-	import * as FormPrimitive from 'formsnap';
-	import { cn } from '$lib/utils.js';
+	import * as FormPrimitive from "formsnap";
+	import { cn } from "$lib/utils.js";
 
 	type $$Props = FormPrimitive.FieldErrorsProps & {
 		errorClasses?: string | undefined | null;
 	};
 
-	let className: $$Props['class'] = undefined;
+	let className: $$Props["class"] = undefined;
 	export { className as class };
-	export let errorClasses: $$Props['class'] = undefined;
+	export let errorClasses: $$Props["class"] = undefined;
 </script>
 
 <FormPrimitive.FieldErrors
+	class={cn("text-destructive text-sm font-medium", className)}
 	{...$$restProps}
-	class={cn('text-sm font-medium text-destructive', className)}
-	let:errorAttrs
 	let:errors
 	let:fieldErrorsAttrs
+	let:errorAttrs
 >
-	<slot {errorAttrs} {errors} {fieldErrorsAttrs}>
+	<slot {errors} {fieldErrorsAttrs} {errorAttrs}>
 		{#each errors as error}
 			<div {...errorAttrs} class={cn(errorClasses)}>{error}</div>
 		{/each}
