b6829059ec7452ea9d5efec23be6e6ac97632e54
PolpOnline <git@t0mmy.anonaddy.com>
2024-09-05 18:28:36 +0200

    Fix bindings/index.ts generator

--- src/backend/build.rs
+++ src/backend/build.rs
@@ -8,28 +8,33 @@ use std::{
 fn main() {
     // trigger recompilation when a new migration is added
     println!("cargo:rerun-if-changed=migrations");
+    println!("cargo::rerun-if-changed=bindings/*");
 
     gen_binding_index();
 }
 
 // From https://github.com/Aleph-Alpha/ts-rs/issues/133#issuecomment-1399589933
 fn gen_binding_index() {
-    println!("generating bindings/index.ts");
+    println!("Generating bindings/index.ts");
 
-    let exports: Vec<_> = fs::read_dir("./bindings")
-        .unwrap()
-        .filter_map(Result::ok)
-        .filter_map(|p| {
-            p.path()
-                .file_stem()
-                .map(OsStr::to_str)
-                .flatten()
-                .map(str::to_owned)
-        })
-        .filter(|f| f != "index")
-        .map(|f| format!("export * from \"./{}\"", f))
-        .collect();
+    if let Ok(exports) = fs::read_dir("./bindings") {
+        let exports: Vec<_> = exports
+            .filter_map(Result::ok)
+            .filter_map(|p| {
+                p.path()
+                    .file_stem()
+                    .and_then(OsStr::to_str)
+                    .map(str::to_owned)
+            })
+            .filter(|f| f != "index")
+            .map(|f| format!("export * from \"./{}\"", f))
+            .collect();
 
-    let mut file = File::create("./bindings/index.ts").unwrap();
-    file.write_all(exports.join("\n").as_bytes()).unwrap();
+        let mut file = File::create("./bindings/index.ts").unwrap();
+        file.write_all(exports.join("\n").as_bytes()).unwrap();
+
+        println!("Generated bindings/index.ts");
+    } else {
+        eprintln!("Cannot generate bindings/index.ts, bindings directory not found");
+    }
 }
