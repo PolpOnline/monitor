24e2e4c7bfd79f7aac4a6c7528424b3a1425d350
PolpOnline <git@t0mmy.anonaddy.com>
2024-09-13 04:01:35 +0200

    Schedule every 15 minutes, not 5 minutes

--- src/backend/src/workers/mod.rs
+++ src/backend/src/workers/mod.rs
@@ -12,7 +12,7 @@ pub async fn register_workers(
     smtp_client: SmtpClient,
 ) -> Result<()> {
     // Add a new periodic job, every 15 minutes
-    periodic::builder("0 */5 * * * *")?
+    periodic::builder("0 */15 * * * *")?
         .name("Check and send emails for down services")
         .queue("down_emails")
         .register(p, EmailWorker::new(db, smtp_client))
