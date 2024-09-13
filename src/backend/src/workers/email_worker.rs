cad34fe150f5c0e4a5ebc8265912a34f24e00d3e
PolpOnline <git@t0mmy.anonaddy.com>
2024-09-13 20:43:42 +0200

    Add UTC to email

--- src/backend/src/workers/email_worker.rs
+++ src/backend/src/workers/email_worker.rs
@@ -76,8 +76,8 @@ fn compose_email(down_service: EmailData) -> GenericResult<Message> {
         .header(ContentType::TEXT_PLAIN)
         .body(
             format!(
-                "Service {} (system id {}) is down since {}. It was supposed to be up after {}. \
-                 Please check it.",
+                "Service {} (system id {}) is down since {} UTC. It was supposed to be up after \
+                 {}. Please check it.",
                 down_service.system_name,
                 down_service.system_id,
                 down_service.timestamp,
