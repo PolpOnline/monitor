cebe4c549345d71547645f6e34ee434c6be13e72
PolpOnline <git@t0mmy.anonaddy.com>
2024-10-05 19:30:39 +0200

    Clearer account settings

--- src/frontend/src/routes/account_management/+page.svelte
+++ src/frontend/src/routes/account_management/+page.svelte
@@ -6,13 +6,15 @@
 	// noinspection ES6UnusedImports
 	import * as Card from '$components/ui/card';
 	import LucideChevronLeft from '~icons/lucide/chevron-left';
+	import LucideRectangleEllipsis from '~icons/lucide/rectangle-ellipsis';
+	import LucideGlobe from '~icons/lucide/globe';
 	import { Button } from '$components/ui/button';
 	title.set('Login');
 
 	export let data: PageData;
 </script>
 
-<div class="mx-3">
+<div class="mx-3 mb-16">
 	<div class="mb-5 mt-3 grid grid-cols-3">
 		<Button href="/" variant="outline" size="icon">
 			<LucideChevronLeft class="h-6 w-6" />
@@ -24,8 +26,9 @@
 	<div class="grid gap-6 md:grid-cols-2">
 		<Card.Root>
 			<Card.Header>
-				<Card.Title>Change Password</Card.Title>
-				<Card.Description>Update your account password here.</Card.Description>
+				<Card.Title>
+					<LucideRectangleEllipsis class="mb-5 h-10 w-10" /> Change Password
+				</Card.Title>
 			</Card.Header>
 			<Card.Content>
 				<ChangePasswordForm data={data.passwordForm} />
@@ -34,8 +37,11 @@
 
 		<Card.Root>
 			<Card.Header>
-				<Card.Title>Change Timezone</Card.Title>
-				<Card.Description>Select your preferred timezone.</Card.Description>
+				<Card.Title>
+					<LucideGlobe class="mb-5 h-10 w-10" />
+					Change Timezone
+				</Card.Title>
+				<Card.Description>Time in emails will be displayed using this.</Card.Description>
 			</Card.Header>
 			<Card.Content>
 				<ChangeTimezoneForm data={data.timezoneForm} timezones={data.timezones} />
