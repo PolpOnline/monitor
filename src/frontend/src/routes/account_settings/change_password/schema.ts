7e048fb287422a3683c4c1878b6ebc6e63af0299
PolpOnline <git@t0mmy.anonaddy.com>
2024-10-06 16:11:25 +0200

    Add custom message to too weak passwords

--- src/frontend/src/routes/account_settings/change_password/schema.ts
+++ src/frontend/src/routes/account_settings/change_password/schema.ts
@@ -3,8 +3,10 @@ import { z } from 'zod';
 export const formSchema = z
 	.object({
 		old_password: z.string().min(8),
-		new_password: z.string().min(8),
-		new_password_confirm: z.string().min(8)
+		new_password: z.string().min(8, { message: 'Password must be at least 8 characters long' }),
+		new_password_confirm: z
+			.string()
+			.min(8, { message: 'Password must be at least 8 characters long' })
 	})
 	.refine((data) => data.new_password === data.new_password_confirm, {
 		message: "Passwords don't match",
