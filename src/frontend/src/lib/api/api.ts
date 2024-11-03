e731e4156933f7f4d04391d45f8ff9d394269c7c
PolpOnline <git@t0mmy.anonaddy.com>
2024-11-03 16:56:55 +0100

    Use env API url

--- src/frontend/src/lib/api/api.ts
+++ src/frontend/src/lib/api/api.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment';
 
-export const API_URL = dev ? 'http://localhost:3000' : 'https://monitor-api.polp.online';
+export const API_URL = dev ? 'http://localhost:3000' : String(import.meta.env.API_URL);
 
 export const LIST_SIZE = 30;
