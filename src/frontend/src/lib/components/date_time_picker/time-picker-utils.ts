f91d91cd1be4ee6766f2d1d62d4ef31ba04a6f0c
PolpOnline <git@t0mmy.anonaddy.com>
2024-11-10 16:37:19 +0100

    Remove unused time-picker-utils.ts

--- src/frontend/src/lib/components/date_time_picker/time-picker-utils.ts
+++
@@ -1,9 +0,0 @@
-// Regular expression to check for valid hour format (01-23)
-export function isValidHour(value: string) {
-	return /^(0[0-9]|1[0-9]|2[0-3])$/.test(value);
-}
-
-// Regular expression to check for valid minute/second format (00-59)
-export function isValidMinuteSecond(value: string) {
-	return /^[0-5][0-9]$/.test(value);
-}
