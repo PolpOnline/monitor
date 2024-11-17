e0efb225524008a900025fcd4fca05d0cacf8657
PolpOnline <git@t0mmy.anonaddy.com>
2024-11-17 02:00:34 +0100

    Reduce animation time on ItemStatus

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -17,7 +17,7 @@
 
 	const inParams: FlyParams = { y: '100%', duration: 300, easing: cubicOut };
 	const outParams: FlyParams = { x: '100%', duration: 700, easing: cubicOut };
-	const animateParams: AnimationConfig = { delay: 700, duration: 300, easing: cubicOut };
+	const animateParams: AnimationConfig = { delay: 0, duration: 300, easing: cubicOut };
 </script>
 
 <svelte:head>
