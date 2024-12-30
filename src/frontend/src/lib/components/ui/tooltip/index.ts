a9cbfcf304b11aeadb98eba64b164965c7071715
PolpOnline <git@t0mmy.anonaddy.com>
2024-12-30 16:52:19 +0100

    Reexport arrow on the trigger

--- src/frontend/src/lib/components/ui/tooltip/index.ts
+++ src/frontend/src/lib/components/ui/tooltip/index.ts
@@ -4,15 +4,18 @@ import Content from "./tooltip-content.svelte";
 const Root = TooltipPrimitive.Root;
 const Trigger = TooltipPrimitive.Trigger;
 const Provider = TooltipPrimitive.Provider;
+const Arrow = TooltipPrimitive.Arrow;
 
 export {
 	Root,
 	Trigger,
 	Content,
 	Provider,
+	Arrow,
 	//
 	Root as Tooltip,
 	Content as TooltipContent,
 	Trigger as TooltipTrigger,
 	Provider as TooltipProvider,
+	Arrow as TooltipArrow
 };
