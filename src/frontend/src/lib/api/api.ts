422608db8119b769f6324651f360efc8e78b0ce9
PolpOnline <git@t0mmy.anonaddy.com>
2025-02-13 15:07:42 +0100

    Check for API_URL environment variable

--- src/frontend/src/lib/api/api.ts
+++ src/frontend/src/lib/api/api.ts
@@ -1,8 +1,10 @@
 import { dev } from '$app/environment';
 import createClient from 'openapi-fetch';
 import type { paths } from '$lib/api/schema';
+import { env } from '$env/dynamic/private';
 
-export const API_URL = dev ? 'http://localhost:3000' : 'https://monitor-api.polp.online';
+export const API_URL =
+	env.API_URL || dev ? 'http://localhost:3000' : 'https://monitor-api.polp.online';
 
 export const LIST_SIZE = 30;
 
